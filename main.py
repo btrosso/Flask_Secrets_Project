3ba75935f554c6fe93cafc742d8318735fd8ab20
btrosso <51491995+btrosso@users.noreply.github.com>
2022-04-19 22:35:54 -0500

    completed project

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 load_dotenv()
 
+
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = os.environ['SECRET_KEY']
 
 class MyForm(FlaskForm):
